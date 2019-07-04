7329b67c42b556c45df9e77017f8aa7aa727702d
oleg <Wickodin@gmail.com>
2019-07-04 17:16:48 +0200

    add .gitignore file, fix UserSearchTest

--- src/main/java/UserSearchTest.java
+++ src/main/java/UserSearchTest.java
@@ -1,19 +1,18 @@
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class UserSearchTest {
 
     @Test
     public void searchUserTest(String idCompany, String name) {
         given().
-            param(idCompany, 777).
-            param(name, "Izergil").
+            param(idCompany, "user_company_id").
+            param(name, "member_name").
         when().
             get("http://some_domain.com/company/" + idCompany + "/users?name=" + name).
         then().
-            body()
-
-
-
+            body(matchesJsonSchemaInClasspath("")).extract().path("company_id").equals("user_company_id");
     }
 }
